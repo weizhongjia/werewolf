cc2c631b9ba3de6beab21fc52212bbbfcfdc895e
richard <zhangruiqian001@hotmail.com>
2017-06-30 00:01:36 +0800

    分数计算狼人

--- src/main/java/com/msh/room/model/role/impl/Werewolves.java
+++ src/main/java/com/msh/room/model/role/impl/Werewolves.java
@@ -4,12 +4,14 @@ import com.msh.room.dto.event.PlayerEventType;
 import com.msh.room.dto.response.PlayerDisplayInfo;
 import com.msh.room.dto.room.RoomStateData;
 import com.msh.room.dto.room.RoomStatus;
+import com.msh.room.dto.room.record.DaytimeRecord;
 import com.msh.room.dto.room.result.GameResult;
 import com.msh.room.dto.room.seat.PlayerSeatInfo;
 import com.msh.room.model.role.Roles;
 import com.msh.room.model.role.util.PlayerRoleMask;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -25,12 +27,38 @@ public class Werewolves extends AssignedPlayer {
 
     @Override
     public void calculateScore() {
-        //TODO 狼人结算
-        int initialScore = 6;
-        PlayerSeatInfo seatInfo = roomState.getPlaySeatInfoBySeatNumber(this.number);
+        int initialScore = 5;
         if (GameResult.WEREWOLVES_WIN.equals(roomState.getGameResult())) {
-            seatInfo.setFinalScore(17);
+            PlayerSeatInfo seatInfo = roomState.getPlaySeatInfoBySeatNumber(this.number);
+            int finalScore = calculateFinalScore(initialScore);
+            if (finalScore > 17) {
+                finalScore = 17;
+            }
+            seatInfo.setFinalScore(finalScore);
         }
+    }
+
+    private int calculateFinalScore(int initialScore) {
+        int finalScore = initialScore;
+        //貌似白天晚上永远一样，因为只能天亮了才能结算Over。
+        int nightSize = roomState.getNightRecordList().size();
+        int dayTimeSize = roomState.getDaytimeRecordList().size();
+        if (dayTimeSize < 3) {
+            finalScore = 17;
+        }
+        if (dayTimeSize == 3) {
+            finalScore = 12;
+        }
+        if (dayTimeSize == 4) {
+            finalScore = 7;
+        }
+        if (dayTimeSize == 5) {
+            finalScore = 6;
+        }
+        if (dayTimeSize >= 6) {
+            finalScore = 3;
+        }
+        return finalScore;
     }
 
     public void explode() {
